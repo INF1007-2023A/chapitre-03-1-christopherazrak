4dce75755274ed0f3fa4739be98153aaee844844
christopherazrak <143291720+christopherazrak@users.noreply.github.com>
2023-10-07 10:33:39 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -1,52 +1,33 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import math 
+
+num=float(input('nombre'))
+print(num**(1/2), 'racine carree')
 
 
-import math
-
-def square_root(a: float) -> float:
-    return 0.0
+a,b,c=float(input('a')),float(input('b')), float(input('c'))
+print((a+b+c)/3)
 
 
-def square(a: float) -> float:
-    return 0.0
+deg,minutes,sec=float(input('degrés')),float(input('minutes')),float(input('secondes'))
+
+degr=deg+minutes/60+sec/3600
+print(degr)
+print(math.radians(degr))
+
+rad=float(input('radians'))
+
+degre=math.degrees(rad)
+
+d=degre//1
+
+m=(degre*60-d*60)//1
+
+s=round((degre*3600-m*60-d*3600),0)
+
+print(d,m,s)
 
 
-def average(a: float, b: float, c: float) -> float:
-    return 0.0
-
-
-def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
-
-
-def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
-
-
-def to_celsius(temperature: float) -> float:
-    return 0.0
-
-
-def to_farenheit(temperature: float) -> float:
-    return 0.0
-
-
-def main() -> None:
-    print(f"La racine carré de 144 est : {square_root(144)}")
-
-    print(f"Le carré de 12 est : {square(12)}")
-
-    print(f"Moyenne des nombres 2, 4, 6: {average(2, 4, 6)}")
-
-    print(f"Conversion de 100 degres, 2 minutes et 45 secondes en radians: {to_radians(180, 2, 45)}")
-    
-    degrees, minutes, seconds = to_degrees(1.0)
-    print(f"Conversion de 1 radian en degres: {degrees} degres, {minutes} minutes et {seconds} secondes")
-
-    print(f"Conversion de 100 Celsius en Farenheit: {to_farenheit(100.0)}")
-    print(f"Conversion de 451 Farenheit en Celsius: {to_celsius(451.0)}")
-
-
-if __name__ == '__main__':
-    main()
+far=float(input('Fahrenheit'))
+cel=(far-32)/1.8
+print(cel)
+print(far)
